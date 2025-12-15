534c36b72846c056a7d9c449a7e24d49913024d9
Rob Sartin <rob.sartin@gmail.com>
2025-12-15 17:54:25 -0600

    Revert "Merge pull request #27 from robsartin/claude/code-review-yaml-config-jwi3T"
    This reverts commit 02f0cecbccb564b65f08eaefd97e71bfd2591ec7, reversing
    changes made to 29643575db98b559bc2533aa8197be2dcb47bde9.

--- src/main/java/com/robsartin/graphs/infrastructure/ImmutableGraph.java
+++ src/main/java/com/robsartin/graphs/infrastructure/ImmutableGraph.java
@@ -1,12 +1,6 @@
 package com.robsartin.graphs.infrastructure;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Consumer;
 
 /**
